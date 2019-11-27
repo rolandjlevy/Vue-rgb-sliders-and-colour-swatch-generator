9a3f8df2b79d987477025969f7b9b7aaf32d0168
Roland Levy <rolandjlevy@gmail.com>
2019-11-27 11:20:35 +0000

    Refactored: made slider component and utils mixin

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+const ColourSlider = {
+  template: '#colour-slider-template',
+  data: () => ({
+    vmodel: 0
+  }),
+  props: {
+    col: {
+      type: String,
+      default: ''
+    }
+  },
+  watch: {
+    vmodel: function (value) {
+      this.$parent.$emit('slidervalue', { col: this.col, value });
+    }
+  }
+}
+
+const UtilsMixin = {
+  methods: {
+    hex(value) {
+      const n = Math.abs(value).toString(16);
+      return n < 10 ? `0${n}` : n.toUpperCase();
+    },
+    randomNum(n) {
+      return Math.round(Math.random(n) * n) -1;
+    },
+    hexColour(r, g, b) {
+      return `${this.hex(r)}${this.hex(g)}${this.hex(b)}`;
+    },
+    randomise() {
+      this.red = this.randomNum(256);
+      this.green = this.randomNum(256);
+      this.blue = this.randomNum(256);
+    }
+  }
+}
+
 const ColourPicker = {
   template: '#colour-picker-template',
   data: () => ({
@@ -7,6 +45,7 @@ const ColourPicker = {
     coloursArray: [],
     random: false
   }),
+  mixins: [UtilsMixin],
   methods: {
     addColour(r, g, b) {
       const id = this.coloursArray.length;
@@ -27,31 +66,20 @@ const ColourPicker = {
     },
     clearAll() {
       this.coloursArray = [];
-    },
-    hex(value) {
-      const n = Math.abs(value).toString(16);
-      return n < 10 ? `0${n}` : n.toUpperCase();
-    },
-    hexColour(r, g, b) {
-      return `${this.hex(r)}${this.hex(g)}${this.hex(b)}`;
-    },
-    randomNum(n) {
-      return Math.round(Math.random(n) * n) -1;
-    },
-    randomise() {
-      this.red = this.randomNum(256);
-      this.green = this.randomNum(256);
-      this.blue = this.randomNum(256);
     }
   },
   mounted() {
     this.randomise();
+    this.$on('slidervalue', ({col, value}) => {
+      this[col] = value;
+    });
   }
 }
 
 new Vue({
   el: "#app",
   components: {
-    ColourPicker
+    ColourPicker,
+    ColourSlider
   }
 });
